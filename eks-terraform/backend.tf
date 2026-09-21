4c1230f3a769b6b43e337c9d015ad62efe032a2a
kalyansagar5 <kalyansagar3001@gmail.com>
2026-09-22 03:23:57 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.6.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.25.0"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
     }
   }
 
@@ -11,6 +17,4 @@ terraform {
     key    = "k8/terraform.tfstate"
     region = "us-east-1"
   }
-
-  required_version = ">= 1.6.3"
 }
