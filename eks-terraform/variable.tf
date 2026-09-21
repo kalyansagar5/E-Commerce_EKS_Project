729903c7306386b67194350db078f9721c62a004
kalyansagar5 <kalyansagar3001@gmail.com>
2026-09-22 03:16:46 +0530

    Update variable.tf

--- eks-terraform/variable.tf
+++ eks-terraform/variable.tf
@@ -1,3 +1,13 @@
+variable "vpc_id" {
+  description = "VPC ID where the EKS cluster will be deployed"
+  type        = string
+
+  validation {
+    condition     = can(regex("^vpc-[a-zA-Z0-9]+$", var.vpc_id))
+    error_message = "vpc_id must be a valid AWS VPC ID."
+  }
+}
+
 variable "node_group_name" {
   description = "Name of the EKS node group"
   type        = string
